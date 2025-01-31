92e162b8818025e517090a1682f78649818633c2
Arya Trivedi <113917940+AryaTrivedi25@users.noreply.github.com>
2025-01-31 21:38:57 +0530

    resolving bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from './sections/Contact/Contact';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Footer from './sections/Footer/Footer';
-import TncModal from './sections/TncModal/tncModal';
+import TncModal from './sections/TncModal/TncModal';
 
 
 function App() {
